04e12e61179b2e30728b1351c1882e23be289e12
mosly <m.mosly90@hotmail.com>
2025-05-02 13:16:42 +0000

    team name

--- webapp/app.py
+++ webapp/app.py
@@ -453,7 +453,7 @@ def main():
     
     # Footer
     st.sidebar.markdown("---")
-    st.sidebar.info("Developed by Mostafa Mosly")
+    st.sidebar.info("Developed by Mostafa Mosly, Zienab Ahmed, Mohamed Abdelgwad, and Nourhan Mowad")
 
 if __name__ == "__main__":
     main()
